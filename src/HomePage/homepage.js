7c825c2f3d4e26672d3bd8761bdd8c23b2693b2d
Tran Anh Tuan <tuantahe179007@fpt.edu.vn>
2024-11-28 00:58:24 +0700

    Update homepage.js

--- src/HomePage/homepage.js
+++ src/HomePage/homepage.js
@@ -1,8 +1,8 @@
 import { Button, Container, Form, Nav, Navbar, Col, Row, Image, Carousel, NavDropdown, InputGroup } from 'react-bootstrap'
 import React, { useEffect, useState } from 'react';
-import Headerhomepage from './Header';
+import Header from '../Header/Header';
 import Main from './Main';
-import Footer from './Footer';
+import Footer from '../Footer/Footer';
 import BXH from './BXH';
 import Carousel1 from './Carousel';
 
@@ -22,7 +22,7 @@ export default function HomePage() {
       <div>
         <Container>
         <Row>
-            <Headerhomepage/>
+            <Header/>
         </Row>
         <Row>
           <Carousel1></Carousel1>
