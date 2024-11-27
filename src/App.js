628e52fc43f3fc5fb1a59cd1504fa049a58e66ab
Tran Anh Tuan <tuantahe179007@fpt.edu.vn>
2024-11-28 00:48:09 +0700

    Fix Login Artist

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage/Homepage';
 import Login from './Login/Login';
+import LoginArtist from './Login/LoginArtist';
 import Register from './Register/Register';
 import Profile from './Profile/Profile';
 import SongDetail from './SongDetails/SongDetails';
 import AddSongArtist from './Artist/AddSong';
 import ManageTableArtist from './Artist/ManagerSong';
+
 
 
 function App() {
@@ -16,6 +18,7 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='/Home' element={<HomePage />} />
           <Route path='/Login' element={<Login />} />
+          <Route path='/logina' element={<LoginArtist />} />
           <Route path='/Register' element={<Register />} />
           <Route path='/profile/:uID' element={<Profile />} />
           <Route path='/userprofile/:uID' element={<Profile />} />
